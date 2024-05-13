127964fef3935eef6e32a8b1c3ef2d38d19b718f
bealqiu(邱彬泳) <1369175442@qq.com>
2024-05-13 17:45:48 +0800

    init:初始化项目

---
+++ src/components/Dock/DockItem.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import {defineProps} from 'vue';
+import { interpolate } from 'popmotion';
+const props = defineProps<{
+  appID?:string
+  title?:string
+  mouseX?:number
+}>()
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="less">
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    123456
+  </div>
+</template>
+
+<style></style>
