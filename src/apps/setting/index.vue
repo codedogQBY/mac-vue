b5faf584aff92b07f8d68413e0be97e1624513eb
qbyyyy <bealqiu@futunn.com>
2024-05-15 00:12:07 +0800

    feat:完善窗口逻辑

---
+++ src/apps/setting/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Window from '@/components/Window/Window.vue';
+const setting = ref([
+  { id: 1, name: 'General' },
+  { id: 2, name: 'Security' },
+  { id: 3, name: 'Privacy' },
+]);
+</script>
+
+<template>
+  <Window title="Setting">
+    <div>
+      <h1>Setting</h1>
+      <p>Here is a list of your setting:</p>
+      <ul>
+        <li v-for="set in setting" :key="set.id">
+          {{ set.name }}
+        </li>
+      </ul>
+    </div>
+  </Window>
+</template>
